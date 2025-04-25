cb00b4df4edf32d3f19f3f8766691738fda1b451
Guilherme Guimarães <guilhermegpereira26@hotmail.com>
2025-04-25 09:28:14 -0300

    Add controller

---
+++ src/main/java/br/com/fiap/arcadia/controller/ItemController.java
@@ -0,0 +1,4 @@
+package br.com.fiap.arcadia.controller;
+
+public class ItemController {
+}
---
+++ src/main/java/br/com/fiap/arcadia/controller/PersonagemController.java
@@ -0,0 +1,77 @@
+package br.com.fiap.arcadia.controller;
+
+import br.com.fiap.arcadia.model.Personagem;
+import br.com.fiap.arcadia.repository.PersonagemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/personagem")
+public class PersonagemController {
+
+    @Autowired
+    private PersonagemRepository repository;
+
+    @GetMapping
+    public List<Personagem> index() {
+        return repository.findAll();
+    }
+
+    @GetMapping("{nome}")
+    public ResponseEntity<List<Personagem>> getNome(@PathVariable String nome) {
+        return ResponseEntity.ok(getPersonagemPorNome(nome));
+    }
+
+    @GetMapping("{classe}")
+    public ResponseEntity<List<Personagem>> getClasse(@PathVariable String classe) {
+        return ResponseEntity.ok(getPersonagemPorClasse(classe));
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Personagem create(@RequestBody Personagem personagem) {
+        return repository.save(personagem);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Personagem> delete(@PathVariable Long id) {
+        repository.delete(getPersonagemPorId(id));
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("{id}")
+    public Personagem update(@PathVariable Long id, @RequestBody Personagem personagem) {
+        personagem.setId(id);
+
+        return repository.save(personagem);
+    }
+
+
+    private Personagem getPersonagemPorId(Long id) {
+        return repository.findById(id)
+                .orElseThrow(
+                        () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Nenhum personagem encontrado com o id " + id)
+                );
+    }
+
+    private List<Personagem> getPersonagemPorClasse(String classe) {
+        return repository.findByClasse(classe)
+                .orElseThrow(
+                        () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Nenhum personagem encontrado com a classe " + classe)
+                );
+    }
+
+    private List<Personagem> getPersonagemPorNome(String nome) {
+        return repository.findByNome(nome)
+                .orElseThrow(
+                        () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Nenhum personagem encontrado com o nome " + nome)
+                );
+    }
+
+
+}
